b060a1d1d8266e5e8151efae7c1bd5f3512a521e
DESKTOP-NP7O4GA\FRCProgrammer1 <studentlaptop1@purdue.pphs.edu>
2026-08-27 14:31:53 -0700

    Delete unused canID's

--- src/main/java/org/supurdueper/robotOffseason/CanId.java
+++ src/main/java/org/supurdueper/robotOffseason/CanId.java
@@ -17,34 +17,7 @@ public enum CanId {
     CANCODER_STEER_FR(22, Constants.canivoreBus),
     CANCODER_STEER_BL(23, Constants.canivoreBus),
     CANCODER_STEER_BR(24, Constants.canivoreBus),
-    PIGEON(25, Constants.canivoreBus),
-    CANDLE(29, Constants.rioBus),
-
-    // Intake
-    INTAKE_ONE(9, Constants.canivoreBus),
-    INTAKE_TWO(10, Constants.canivoreBus),
-
-    // Hopper
-    INDEXER_ONE(11, Constants.canivoreBus),
-    INDEXER_TWO(12, Constants.canivoreBus),
-
-    // Feeder
-    FEEDER_ONE(13, Constants.canivoreBus),
-    FEEDER_TWO(14, Constants.canivoreBus),
-
-    // Shooter Hood
-    SHOOTER_HOOD(15, Constants.canivoreBus),
-    CANCODER_HOOD(26, Constants.canivoreBus),
-
-    // Shooter
-    SHOOTER_ONE(16, Constants.canivoreBus),
-    SHOOTER_TWO(17, Constants.canivoreBus),
-    SHOOTER_THREE(18, Constants.canivoreBus),
-    SHOOTER_FOUR(19, Constants.canivoreBus),
-    SHOOTER_FIVE(20, Constants.canivoreBus),
-
-    // CLIMBER
-    CLIMBER(21, Constants.canivoreBus);
+    PIGEON(25, Constants.canivoreBus);
 
     private final int mDeviceNumber;
     private final CANBus mBus;
